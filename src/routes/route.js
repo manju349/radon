41e5f9ba64c091c3c1a2731e20fb129d8b126e83
manju349 <manjunath349@gmail.com>
2022-06-14 21:41:41 +0530

    changes

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const userController= require("../controllers/userController")
+const middleware= require("../middleware/auth")
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
-router.post("/users", userController.createUser  )
-
+router.post("/users", userController.createUser)
 router.post("/login", userController.loginUser)
 
-//The userId is sent by front end
-router.get("/users/:userId", userController.getUserData)
+router.put("/users/:userId", middleware.mid1, userController.updateUser)
+router.get("/getusers/:userId", middleware.mid1, userController.getUserData)
 
-router.put("/users/:userId", userController.updateUser)
+//The userId is sent by front end
+router.delete("/users/:userId", middleware.mid1, userController.deleteUser)
+
 
 module.exports = router;
